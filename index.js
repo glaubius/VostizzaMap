4eb395772e2f840c7472037bfdd90cd60c2a7d1d
Jen Glaubius <glaubius@gmail.com>
2020-03-17 09:58:46 -0400

    add leaflet map basics

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 /*jshint esversion: 6 */
+
+console.log('L', L);
+
+const position = [38.2500, 22.0833];
+const initZoom = 9;
+
+const layer = new L.StamenTileLayer("terrain");
+const map = L.map('map').setView(position, initZoom);
+
+map.addLayer(layer);
+
+console.log('map', map);
