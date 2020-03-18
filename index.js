21aa5c09efa33f79fcdc76e06d302236c5fe639c
Jen Glaubius <glaubius@gmail.com>
2020-03-18 10:09:16 -0400

    add data and leaflet-ajax

--- index.js
+++ index.js
@@ -11,24 +11,9 @@ const map = L.map('map').setView(position, initZoom);
 
 map.addLayer(layer);
 
-// Delete after testing
-var settlements = {
-"type": "FeatureCollection",
-"name": "settlements",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Name": null, "Type": "Inhabited Villa", "ElevationC": 1 }, "geometry": { "type": "Point", "coordinates": [ 38.265925509843164 , 22.017121161639061] } },
-{ "type": "Feature", "properties": { "Name": null, "Type": "Inhabited Zeog", "ElevationC": 2 }, "geometry": { "type": "Point", "coordinates": [ 38.253495229637792,  22.006893381402847 ] } }
-]
-};
+// Add settlements to map
+var settlements = new L.GeoJSON.AJAX("data/settlements-USE.geojson");
+settlements.addTo(map);
 
-var settlementsStyle = {
-  "color": "#ff0000",
-  "weight": 8,
-};
-
-L.geoJSON(settlements, {
-  style: settlementsStyle
-}).addTo(map);
 
 console.log('map', map);
