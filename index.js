433b62aff981a12d6f2a2d3a1fd082e244c322b5
Jen Glaubius <glaubius@gmail.com>
2020-03-18 11:59:55 -0400

    add mapbox tile layer

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ console.log('L', L);
 const position = [38.2500, 22.0833];
 const initZoom = 9;
 const map = L.map('map').setView(position, initZoom);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2xhdWJpdXMiLCJhIjoiY2sxcDV5dHphMHM2bzNvbnFvdW1taHhlNCJ9.mvLNCSMEXScDHVPQBhyS2Q'
+}).addTo(map);
