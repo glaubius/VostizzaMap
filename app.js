3e6fcd21c0586535749769747a9a86ab6401c804
Jen Glaubius <glaubius@gmail.com>
2020-04-14 16:51:48 -0400

    add geojson schema

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const GeoJSON = require("mongoose-geojson-schema");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -13,18 +14,35 @@ mongoose.connect("mongodb://localhost:27017/VostizzaDB", {
   useUnifiedTopology: true
 });
 
-const locationSchema = new mongoose.Schema({
-  type: String,
+const locationsSchema = new mongoose.Schema({
+  type: { type: String },
   properties: {
     name: String,
-    type: String,
+    type: { type: String },
     inhabited: Boolean
   },
   geometry: {
-    type: String,
-    coordinates: [Number, Number]
+    type: { type: String },
+    coordinates: [mongoose.Decimal1128, mongoose.Decimal1128]
   }
 });
+
+const Location = mongoose.model("Location", locationsSchema);
+
+const place = new Location ({
+  type: "Feature",
+  properties: {
+    name: "Paleo Castro",
+    type: "Castro",
+    inhabited: false
+  },
+  geometry: {
+    type: "Point",
+    coordinates: [21.990027723810478, 38.225084511992108]
+  }
+});
+
+place.save();
 
 
 app.get("/", function(req, res) {
